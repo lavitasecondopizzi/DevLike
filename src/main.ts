69c56c064907bc0b174d3c9af9c281f3c70d0350
Emanuele Capizzi <74193846+lavitasecondopizzi@users.noreply.github.com>
2026-09-17 16:47:17 +0200

    Preserve the current screen when opening equipment manager

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,9 @@ import { Game } from "./game/Game";
 import { render } from "./ui/render";
 
 const game = new Game();
+const openEquipment = game.openEquipment.bind(game);
+game.openEquipment = () => {
+  if (game.screen !== "equipment") game.equipmentReturnScreen = game.screen;
+  openEquipment();
+};
 render(game);
