9a20614367b1b998f65213302d384c8e481c3a36
Emanuele Capizzi <74193846+lavitasecondopizzi@users.noreply.github.com>
2026-09-17 17:14:46 +0200

    Use developer-specific starter deck

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import "./extra.css";
 import "./extra2.css";
 import { Game } from "./game/Game";
+import { starterDeckForDeveloper } from "./data/cards";
 import { render } from "./ui/render";
 
 const game = new Game();
@@ -9,5 +10,11 @@ const openEquipment = game.openEquipment.bind(game);
 game.openEquipment = () => {
   if (game.screen !== "equipment") game.equipmentReturnScreen = game.screen;
   openEquipment();
+};
+const confirmStartingDeveloper = game.confirmStartingDeveloper.bind(game);
+game.confirmStartingDeveloper = () => {
+  confirmStartingDeveloper();
+  const active = game.team[0];
+  if (active) game.deck = starterDeckForDeveloper(active.id);
 };
 render(game);
